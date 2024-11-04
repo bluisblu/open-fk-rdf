5d71469f3b3e80130fef4563feb25650c7305f3a
bluisblu <naynuza19@gmail.com>
2024-11-04 17:48:06 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# open-fk-rdf
+An encoding/decoding library for .rdf files, ported from [GittyMac/OpenFK](https://github.com/GittyMac/OpenFK/) into rust.
---
+++ examples/file.rs
@@ -0,0 +1,13 @@
+use std::{fs::File, io::{self, Read}};
+use open_fk_rdf::{decode_rdf, encode_xml};
+
+fn main() -> io::Result<()> {
+    let mut file = File::open("profile.rdf")?;
+    let mut encoded_data = Vec::new();
+    file.read_to_end(&mut encoded_data)?;
+
+    let decoded_xml = decode_rdf(&encoded_data);
+    println!("Decoded XML: {}", decoded_xml);
+
+    Ok(())
+}
